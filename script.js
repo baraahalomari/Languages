6c48bb8941eadd9ca51197a8faa2ae706ebb338e
Baraah Alomari <balomari995@gmail.com>
2021-03-18 15:16:31 +0200

    I added java script interaction to my website

---
+++ script.js
@@ -0,0 +1,25 @@
+var userName=prompt ("what's your Name?");
+
+var languages=prompt("How many language do you speak");
+
+if (languages >= 2){
+    alert("Great job");
+}
+else 
+{
+   alert( "keep learning" );
+}
+
+var favoriteLanguage = prompt("what is your favorite language");
+
+if ( favoriteLanguage == "English"){
+    
+    document.write("<img src='https://www.csss.es/wp-content/uploads/2018/10/English-languages.jpg' alt='english' />");
+    document.write("Over time, you will start hearing about the important things to learn to be able to effectively do business. You’ll be told about certain kinds of courses and jobs/internships to pursue that’ll benefit your pursuit in the business life. These are things that will benefit you as a local business pursuer, and even for someone interested in pursuing international business, these things can be beneficial");
+}
+else if(favoriteLanguage == "Arabic"){ 
+   
+    document.write("<img src='https://i1.wp.com/www.arabamerica.com/wp-content/uploads/2019/12/63652739575886387531.jpg?w=730&ssl=1' alt='arabic' />");
+}else {
+    alert("choose between English or Arabic");
+} 
