0d58c221d1754f6761988a1145c4cdce29946b9b
Baraah Alomari <balomari995@gmail.com>
2021-03-21 15:09:33 +0200

    adding some condition

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-var userName=prompt ("what's your Name?");
+var userName = prompt("what's your Name?");
 
-var languages=prompt("How many language do you speak");
+var languages = prompt("How many language do you speak");
 
-if (languages >= 2){
+if (languages >= 2) {
     alert("Great job");
 }
-else 
-{
-   alert( "keep learning" );
+else {
+    alert("keep learning");
 }
 
 var favoriteLanguage = prompt("what is your favorite language");
 
-if ( favoriteLanguage == "English"){
-    
+if (favoriteLanguage == "english") {
+
     document.write("<img src='https://www.csss.es/wp-content/uploads/2018/10/English-languages.jpg' alt='english' />");
     document.write("Over time, you will start hearing about the important things to learn to be able to effectively do business. You’ll be told about certain kinds of courses and jobs/internships to pursue that’ll benefit your pursuit in the business life. These are things that will benefit you as a local business pursuer, and even for someone interested in pursuing international business, these things can be beneficial");
 }
-else if(favoriteLanguage == "Arabic"){ 
-   
+else if (favoriteLanguage == "arabic") {
+
     document.write("<img src='https://i1.wp.com/www.arabamerica.com/wp-content/uploads/2019/12/63652739575886387531.jpg?w=730&ssl=1' alt='arabic' />");
-}else {
+} else {
     alert("choose between English or Arabic");
-} 
+}
+
+while (favoriteLanguage !== "english" && favoriteLanguage !== "arabic") {
+    favoriteLanguage = prompt("please input only English or Arabic");
+}
+
+var yourRate = prompt("how much do you rate your languages");
+var result = " ";
+var paint = " ";
+
+if (yourRate <= 10 && favoriteLanguage == "english") {
+    paint = " <img src='https://www.csss.es/wp-content/uploads/2018/10/English-languages.jpg' alt='english'> ";
+} else if (yourRate <= 10 && favoriteLanguage === "arabic") {
+    paint = " <img src='https://i1.wp.com/www.arabamerica.com/wp-content/uploads/2019/12/63652739575886387531.jpg?w=730&ssl=1' alt='arabic' />  ";
+    console.log('arabic');
+}
+
+for (var i = 0; i < yourRate; i++){
+    result += paint;
+}
+document.write(result); 
+console.log(i)
